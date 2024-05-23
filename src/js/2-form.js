2868dbcdfc92e4e82f5c78fab5adf4f296a1f85d
Sveta28U <152779991+Sveta28U@users.noreply.github.com>
2024-05-23 13:36:19 +0300

    commit 2

---
+++ src/js/2-form.js
@@ -0,0 +1,7 @@
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', onInput);
+
+form.addEventListener('submit', onSubmit);
+
+document.addEventListener('DOMContentLoaded', renderPage);
