8fe2cd25fef62bb2de6b5d6d7cc49a8343812667
Sveta28U <152779991+Sveta28U@users.noreply.github.com>
2024-05-29 15:12:01 +0300

    commit task 2

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -5,3 +5,32 @@ form.addEventListener('input', onInput);
 form.addEventListener('submit', onSubmit);
 
 document.addEventListener('DOMContentLoaded', renderPage);
+const LS_KEY = 'feedback-form-state';
+function onInput(event) {
+  const { email, message } = event.currentTarget.elements;
+  const formData = {
+    email: email.value.trim(),
+    message: message.value.trim(),
+  };
+  localStorage.setItem(LS_KEY, JSON.stringify(formData));
+}
+function onSubmit(event) {
+  event.preventDefault();
+  const { email, message } = event.currentTarget.elements;
+  if (email.value.trim() === '' || message.value.trim() === '') {
+    return alert('Fill please all fields');
+  }
+  console.log({
+    email: email.value.trim(),
+    message: message.value.trim(),
+  });
+  localStorage.removeItem(LS_KEY);
+  event.currentTarget.reset();
+}
+function renderPage() {
+  const formData = JSON.parse(localStorage.getItem(LS_KEY));
+  if (formData) {
+    form.elements.email.value = formData.email;
+    form.elements.message.value = formData.message;
+  }
+}
